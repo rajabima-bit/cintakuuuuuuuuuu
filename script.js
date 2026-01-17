d0b7951e80e404623c412bb1de9bf4196d14e7f9
rajabima-bit <rjabima@gmail.com>
2026-01-17 08:57:34 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const password = "sayang";
+const music = document.getElementById("music");
+
+function checkPassword() {
+  const input = document.getElementById("passwordInput").value;
+
+  if (input === password) {
+    document.getElementById("loginPage").classList.remove("active");
+    document.getElementById("mainPage").classList.add("active");
+
+    music.play();
+    startHearts();
+  } else {
+    alert("Password salah 😢");
+  }
+}
+
+function nextMessage() {
+  document.getElementById("mainPage").classList.remove("active");
+  document.getElementById("finalPage").classList.add("active");
+}
+
+// LOVE ANIMATION
+function startHearts() {
+  setInterval(() => {
+    const heart = document.createElement("span");
+    heart.innerHTML = "💖";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 10 + "px";
+    document.getElementById("hearts").appendChild(heart);
+
+    setTimeout(() => heart.remove(), 5000);
+  }, 300);
+}
+
+// ZOOM FOTO
+document.querySelectorAll(".photo-card img").forEach(img => {
+  img.addEventListener("click", () => {
+    document.getElementById("photoModal").style.display = "flex";
+    document.getElementById("modalImg").src = img.src;
+  });
+});
+
+function closeModal() {
+  document.getElementById("photoModal").style.display = "none";
+}
